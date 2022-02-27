6a12475e70f349189a0c04a8fd6f07907a262339
ludoguenet <ludovicguenet@gmx.com>
2022-02-27 15:32:31 +0100

    order controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(OrderController::class)->group(function (){
+    Route::post('orders', 'store');
+    Route::get('orders/{order}', 'show');
+    Route::delete('orders/{order}', 'destroy');
+});
+
